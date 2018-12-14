ff37324c1207441107845e0fe8b776bb123231e2
thrive <thrive@myself.local>
2018-12-14 15:26:17 +0800

    后端验证数据

---
+++ server/routes/users.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import isEmpty from 'lodash/isEmpty';
+import validator from 'validator';
+let router = express.Router();
+const validateInput =(data)=>{
+    let errors={};
+    if (validator.isEmpty(data.username)) {
+        errors.username = "The field is required";
+      }
+    
+      if (validator.isEmpty(data.email)) {
+        errors.email = "The field is required";
+      }
+      if (validator.isEmpty(data.password)) {
+        errors.password = "The field is required";
+      }
+    
+      if (validator.isEmpty(data.passwordConfirmation)) {
+        errors.passwordConfirmation = "The field is required";
+      }
+      if (!validator.equals(data.password, data.passwordConfirmation)) {
+        errors.passwordConfirmation = "Passwords must match";
+      }
+    
+    return{
+        errors,
+        isValid: isEmpty(errors)
+    }
+}
+router.post('/',(req,res)=>{
+    const {errors, isValid} = validateInput(req.body);
+    if(!isValid){
+        res.status(400).json(errors)
+    }
+})
+export default router;
